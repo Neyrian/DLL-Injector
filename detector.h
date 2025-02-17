5c838d936920889a993e7f71ce874b1dbb1dd773
Alexis Lanquetin <alexis.lanquetin@gmail.com>
2025-02-17 18:19:49 +0100

    Update detector.h

--- detector.h
+++ detector.h
@@ -3,27 +3,23 @@
 
 #include <windows.h>
 #include <stdbool.h>
+#include <winternl.h>
+#include <stdio.h>
+#include <tlhelp32.h>
+#include <time.h>
+#include <psapi.h>
+#include <wincrypt.h>
+#include <shlwapi.h>
+
+#define FLG_HEAP_ENABLE_TAIL_CHECK 0x10
+#define FLG_HEAP_ENABLE_FREE_CHECK 0x20
+#define FLG_HEAP_VALIDATE_PARAMETERS 0x40
 
 // Function to launch calc.exe (for testing sandbox evasion)
 void LaunchCalc();
 
-// EDR Detection
-bool DetectEDRs();
-
-// Sleep Patching Detection
-bool DetectSleepPatching();
-
-// Sandbox File Detection
-bool DetectSandboxFiles();
-
-// Packed Execution Detection (Filename Hash)
-bool DetectFilenameHash();
-
-//Detect SandBox DLLs
-bool DetectDLLs();
-
 // Main function to check all sandbox evasion techniques
-bool CheckSandbox();
+bool PerfomChecksEnv();
 
 
 #endif // DETECTOR_H
